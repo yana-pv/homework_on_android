e93d3629540de4135c47ccc29dea3bfcb81da246
Яна Паймухина <yanapaim.vita@gmail.com>
2025-11-30 03:11:27 +0300

    Deleted HomeWork3

--- HomeWork3/app/src/main/java/com/example/homework3/model/UserMessage.kt
+++
@@ -1,9 +0,0 @@
-package com.example.homework3.model
-
-import java.util.Date
-
-data class UserMessage(
-    val text: String,
-    val timestamp: Date = Date(),
-    val isFromReply: Boolean = false
-)
--- HomeWork3/app/src/main/java/com/example/homework3/navigation/NavigationIds.kt
+++
@@ -1,7 +0,0 @@
-package com.example.homework3.navigation
-
-enum class NavigationIds {
-    NOTIFICATION_SETTINGS,
-    NOTIFICATION_EDIT,
-    USER_MESSAGES
-}
